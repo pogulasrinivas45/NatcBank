db5c348baf493acbb603775862aa56f419b4a590
vasu <vasu@gmail.com>
2021-04-22 09:44:30 +0530

    implement display method

--- EmployeeInfo/src/com/sreenu/impl/EmployeeImpl.java
+++ EmployeeInfo/src/com/sreenu/impl/EmployeeImpl.java
@@ -5,7 +5,16 @@ import com.sreenu.bean.Employee;
 public class EmployeeImpl {
 	public void display(Employee e)
 	{
-		
+		System.out.println(e.getEid()+" "+e.getEname()+" "+e.getSalary());
+	}
+	public static void main(String[] args)
+	{
+		EmployeeImpl emp=new EmployeeImpl();
+		Employee e=new Employee();
+		e.setEid(1);
+		e.setEname("Balu");
+		e.setSalary(35000);
+		emp.display(e);
 	}
 
 }
